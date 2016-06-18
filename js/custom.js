48bcf96859c6de0159939ffd2b0344a8f8d49974
Isaac Addis <isaacaddis21@gmail.com>
2016-06-17 20:48:36 -0700

    Homepage (almost) Done! :grinning:

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,5 @@
     $(document).ready(function(){
-<<<<<<< HEAD
       $('.slider').slider();
-=======
-      $('.slider').slider({height:"600px"});
->>>>>>> origin/master
       $(".button-collapse").sideNav();
 
     });
